cab7b4cb187e58cdaa6fd6e6184c0f31413445bd
aseduto <aseduto@hotmail.com>
2016-08-17 13:34:52 +0200

    Updated Stop-VMS.ps1 in Azure Automation.

--- Runbook/Stop-VMS.ps1
+++ Runbook/Stop-VMS.ps1
@@ -1,18 +1,18 @@
-param($stop = $true
-	, $TenantId = '8b07f925-16b1-4b2a-bac8-4698520516b2'
-	, $rg = "NPO-Torino-Azure"
-	, $credname = 'NPOServiceAccount'
+param([bool] $stop = $true
+	, [String] $TenantId = '8b07f925-16b1-4b2a-bac8-4698520516b2'
+	, [String] $rg = "NPO-Torino-Azure"
+	, [String] $credname = 'NPOServiceAccount'
 )
 
 $ErrorActionPreference = "Stop";
 $my_dir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-
 
 $cred = Get-AutomationPSCredential -Name $credname
 
 Login-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId $TenantId | fl 
 
 "-----------------------------" 
+"*****************************" | oh
 
 #get-azureRmSubscription 
 
@@ -22,11 +22,11 @@ $vms = Get-AzureRmVM -ResourceGroupName $rg
 
 if($stop)
 {
-	$vms | foreach{$_ | Stop-AzureRmVm -force}
+	$vms | foreach{"Stop $($_.Name)"; $_ | Stop-AzureRmVm -force}
 }
 else
 {
-	$vms | foreach{$_ | Start-AzureRmVm -force}
+	$vms | foreach{"Start $($_.Name)"; $_ | Start-AzureRmVm -force}
 }
 
 $vms | foreach {$_ | get-azurermvm -status} | foreach {"$($_.Name)`t$($_.Statuses[$_.Statuses.Count - 1].DisplayStatus)"}
